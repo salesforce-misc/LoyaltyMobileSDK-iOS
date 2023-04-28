57e4e1a4f5f751de195429c4c4894b3c32f2a5b3
Leon Qi <lqi@salesforce.com>
2023-04-28 03:54:50 -0700

    Added more comments to each file. Generated docs using `jazzy`

--- Sources/LoyaltyMobileSDK/Extensions/DateFormatter.swift
+++ Sources/LoyaltyMobileSDK/Extensions/DateFormatter.swift
@@ -7,14 +7,19 @@
 
 import Foundation
 
+/// An extension of DateFormatter to provide custom date formats for the Salesforce API.
 public extension DateFormatter {
     
+    /// Enumeration for representing the length of the date format.
     enum Length: String {
         case short = "yyyy-MM-dd"
         case long = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZ"
         case middle = "yyyy-MM-dd'T'HH:mm:ss"
     }
     
+    /// Creates a DateFormatter instance with a specified date format length.
+    /// - Parameter length: The `Length` of the date format (defaults to `.middle`).
+    /// - Returns: A DateFormatter instance with the specified date format length.
     static func forceFormatter(_ length: Length = .middle) -> DateFormatter {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
--- Sources/LoyaltyMobileSDK/ForceSwift/NetworkManager.swift
+++ Sources/LoyaltyMobileSDK/ForceSwift/NetworkManager.swift
@@ -7,16 +7,34 @@
 
 import Foundation
 
+/// A protocol that defines the requirements for a network manager.
 public protocol NetworkManagerProtocol {
+    /// Fetch data from the network and decode it to the specified type.
+    ///
+    /// - Parameters:
+    ///   - type: The expected data type to decode the response into.
+    ///   - request: The URLRequest to be executed by URLSession.
+    ///   - urlSession: An instance of URLSession to execute the request. Defaults to URLSession.shared.
+    ///
+    /// - Returns: A decoded JSON response result.
     func fetch<T: Decodable>(type: T.Type, request: URLRequest, urlSession: URLSession) async throws -> T
 }
 
+/// A class that handles network requests and data processing.
 public class NetworkManager: NetworkManagerProtocol {
     
+    /// A shared instance of NetworkManager for convenience.
     public static let shared = NetworkManager()
     
+    /// Private initializer to prevent multiple instances.
     private init() {}
     
+    /// Handle data and response from a URLSession request.
+    ///
+    /// - Parameter output: A tuple containing the Data and URLResponse from the request.
+    ///
+    /// - Returns: Validated data from the request.
+    /// - Throws: A CommonError if there's an issue with the response or data.
     internal func handleDataAndResponse(output: (data: Data, response: URLResponse)) throws -> Data {
         guard let httpResponse = output.response as? HTTPURLResponse else {
             Logger.error(CommonError.requestFailed(message: "Invalid response").description)
@@ -42,11 +60,15 @@ public class NetworkManager: NetworkManagerProtocol {
         return output.data
     }
 
-    /// Use Async/Await to fetch all REST requests
+    /// Use Async/Await to fetch all REST requests.
+    ///
     /// - Parameters:
-    ///   - type: A type(i.e. model) defined to be used by JSON decoder
-    ///   - request: A URLRequest to be executed by URLSession
-    /// - Returns: A decoded JSON response result
+    ///   - type: A type (i.e., model) defined to be used by JSON decoder.
+    ///   - request: A URLRequest to be executed by URLSession.
+    ///   - urlSession: An instance of URLSession to execute the request. Defaults to URLSession.shared.
+    ///
+    /// - Returns: A decoded JSON response result.
+    /// - Throws: An error if the request or decoding fails.
     public func fetch<T: Decodable>(type: T.Type, request: URLRequest, urlSession: URLSession = URLSession.shared) async throws -> T {
       
         do {
