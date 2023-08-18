1b178af848744aa8fe25832aaf3f2555fdf0fe3c
Vasanthkumar Velusamy <vvelusamy@salesforce.com>
2023-08-18 15:40:31 +0530

    Code refactored and added test cases

--- SampleApps/MyNTORewards/MyNTORewards/ReceiptScanning/Receipts/ViewModels/ReceiptListViewModel.swift
+++ SampleApps/MyNTORewards/MyNTORewards/ReceiptScanning/Receipts/ViewModels/ReceiptListViewModel.swift
@@ -8,6 +8,11 @@
 import Foundation
 import LoyaltyMobileSDK
 
+enum SortOrder: String {
+	case ASC
+	case DESC
+}
+
 @MainActor
 class ReceiptListViewModel: ObservableObject {
 	@Published var receipts: [Receipt] = []
@@ -18,15 +23,26 @@ class ReceiptListViewModel: ObservableObject {
 			filter(query: searchText)
 		}
 	}
-	
-	private let receipts_query = "select Id,Purchase_Date__c,ReceiptId__c,Name,Status__c,StoreName__c,Total_Points__c,CreatedDate,TotalAmount__c from Receipts__c Order by CreatedDate DESC"
-	private let authManager: ForceAuthManager
+	let queryFields = ["Id",
+					   "Purchase_Date__c",
+					   "ReceiptId__c",
+					   "Name",
+					   "Status__c",
+					   "StoreName__c",
+					   "Total_Points__c",
+					   "CreatedDate",
+					   "TotalAmount__c"]
+	let recordName = "Receipts__c"
+	let orderByField = "CreatedDate"
+	let sortOrder = SortOrder.DESC
+	private let authManager: ForceAuthenticator
 	private var forceClient: ForceClient
 	private let localFileManager: FileManagerProtocol
 	private let receiptsListFolderName = "ReceiptsList"
 	
-	init(localFileManager: FileManagerProtocol = LocalFileManager.instance,
-		authManager: ForceAuthManager = .shared,
+	init(
+		localFileManager: FileManagerProtocol = LocalFileManager.instance,
+		authManager: ForceAuthenticator = ForceAuthManager.shared,
 		forceClient: ForceClient? = nil
 	) {
 		self.localFileManager = localFileManager
@@ -35,7 +51,11 @@ class ReceiptListViewModel: ObservableObject {
 	}
 	
 	func filter(query: String) {
-		filteredReceipts = query.isEmpty ? receipts : receipts.filter { $0.receiptId.contains(query) }
+		filteredReceipts = query.trimmingCharacters(in: CharacterSet(charactersIn: " ")).isEmpty ? receipts : receipts.filter { $0.receiptId.contains(query) }
+	}
+	
+	func getQuery() -> String {
+		"select \(queryFields.joined(separator: ",")) from \(recordName) Order by \(orderByField) \(sortOrder.rawValue)"
 	}
 	
 	func getReceipts(membershipNumber: String, forced: Bool = false) async throws {
@@ -47,7 +67,7 @@ class ReceiptListViewModel: ObservableObject {
 			receipts = cached
 		} else {
 			do {
-				let queryResult = try await forceClient.SOQL(type: Receipt.self, for: receipts_query)
+				let queryResult = try await forceClient.SOQL(type: Receipt.self, for: getQuery())
 				receipts = queryResult.records
 				localFileManager.saveData(item: receipts, id: membershipNumber, folderName: receiptsListFolderName, expiry: .never)
 			} catch {
