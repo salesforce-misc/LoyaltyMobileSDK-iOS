c88a1b4db6ffbd61580c84f222a2f2db906f0990
Vasanthkumar Velusamy <vvelusamy@salesforce.com>
2023-09-20 16:11:37 +0530

    Refactored ProcessedReceiptView to be more readable and maintainable

--- SampleApps/MyNTORewards/MyNTORewards/ReceiptScanning/UploadReceipt/Views/ProcessedReceiptView.swift
+++ SampleApps/MyNTORewards/MyNTORewards/ReceiptScanning/UploadReceipt/Views/ProcessedReceiptView.swift
@@ -19,78 +19,11 @@ struct ProcessedReceiptView: View {
         if let processedReceipt = viewModel.processedReceipt {
 			ZStack {
 				VStack {
-					VStack(alignment: .leading, spacing: 20) {
-						Text("Receipt \(processedReceipt.receiptNumber)")
-							.font(.offerTitle)
-							.accessibilityIdentifier(AppAccessibilty.Receipts.receiptNumberLabel)
-						HStack {
-							Text("Store: \(processedReceipt.storeName)")
-								.accessibilityIdentifier(AppAccessibilty.Receipts.storeLabel)
-							Spacer()
-							Text("Date: \(processedReceipt.receiptDate)")
-								.accessibilityIdentifier(AppAccessibilty.Receipts.receiptDate)
-						}
-						.font(.offerText)
-					}
-					.padding(.horizontal)
-					VStack(spacing: 0) {
-						Rectangle()
-							.strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [4, 6]))
-							.frame(height: 1)
-							.padding()
-						
-						ReceiptTableTitleRow()
-							.font(.receiptItemsTitleFont)
-						Rectangle()
-							.strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [4, 6]))
-							.frame(height: 1)
-							.padding()
-						ScrollView {
-							ProcessedReceiptList(items: processedReceipt.lineItem)
-							Rectangle()
-								.strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [4, 6]))
-								.frame(height: 1)
-								.padding()
-						}
-						.frame(maxHeight: .infinity)
-					}
-					.background(.white)
-					.cornerRadius(4)
-					.padding()
+					header(receipt: processedReceipt)
+					table(receipt: processedReceipt)
 					Spacer()
-					Text(StringConstants.Receipts.submitButton)
-						.onTapGesture {
-							Task {
-								do {
-									guard let receiptSFDCId = processedReceipt.receiptSFDCId else { return }
-									isLoading = true
-									let success = try await viewModel.submitForProcessing(receiptId: receiptSFDCId)
-									if success {
-										viewModel.receiptState = .submitted
-										try await receiptlistViewModel.getReceipts(membershipNumber: rootViewModel.member?.membershipNumber ?? "", forced: true)
-									}
-									isLoading = false
-								} catch {
-									isLoading = false
-								}
-							}
-						}
-						.longFlexibleButtonStyle()
-						.accessibilityIdentifier(AppAccessibilty.Receipts.submitReceiptButton)
-					Button(StringConstants.Receipts.tryAgainButton) {
-						// button action
-						Task {
-							routerPath.presentedSheet = nil
-							do {
-								try await receiptlistViewModel.getReceipts(membershipNumber: rootViewModel.member?.membershipNumber ?? "", forced: true)
-							}
-						}
-						DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-							cameraModel.showCamera = true
-						}
-					}
-					.foregroundColor(.black)
-					.accessibilityIdentifier(AppAccessibilty.Receipts.tryAgainButtonProcessedReceipt)
+					submitButton(receipt: processedReceipt)
+					tryAgainButton
 				}
 				.padding(.vertical, 20)
 				.background(Color.theme.background)
@@ -101,33 +34,117 @@ struct ProcessedReceiptView: View {
 						.opacity(0.7)
 				}
 			}
-        } else {
-            VStack {
-                Spacer()
-                ProcessingErrorView(message1: StringConstants.Receipts.processingErrorMessageLine1,
-                                    message2: StringConstants.Receipts.processingErrorMessageLine2)
-                Spacer()
-                Spacer()
-                Text(StringConstants.Receipts.tryAgainButton)
-                    .onTapGesture {
-                        // button action
-                        routerPath.presentedSheet = nil
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                            cameraModel.showCamera = true
-                        }
-                    }
-                    .longFlexibleButtonStyle()
-                Button {
-                    routerPath.presentedSheet = nil
-                } label: {
-                    Text(StringConstants.Receipts.backButton)
-                        .foregroundColor(.black)
-                }
-                .padding(.bottom, 20)
-                .accessibilityIdentifier(AppAccessibilty.Receipts.backButton)
-            }
-            
-        }
+        } else { errorView }
+	}
+	
+	func header(receipt: ProcessedReceipt) -> some View {
+		VStack(alignment: .leading, spacing: 20) {
+			Text("Receipt \(receipt.receiptNumber)")
+				.font(.offerTitle)
+				.accessibilityIdentifier(AppAccessibilty.Receipts.receiptNumberLabel)
+			HStack {
+				Text("Store: \(receipt.storeName)")
+					.accessibilityIdentifier(AppAccessibilty.Receipts.storeLabel)
+				Spacer()
+				Text("Date: \(receipt.receiptDate)")
+					.accessibilityIdentifier(AppAccessibilty.Receipts.receiptDate)
+			}
+			.font(.offerText)
+		}
+		.padding(.horizontal)
+	}
+	
+	func table(receipt: ProcessedReceipt) -> some View {
+		VStack(spacing: 0) {
+			dottedLine
+			ReceiptTableTitleRow()
+				.font(.receiptItemsTitleFont)
+			dottedLine
+			ScrollView {
+				ProcessedReceiptList(items: receipt.lineItem)
+				dottedLine
+			}
+			.frame(maxHeight: .infinity)
+		}
+		.background(.white)
+		.cornerRadius(4)
+		.padding()
+	}
+	
+	var dottedLine: some View {
+		Rectangle()
+			.strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [4, 6]))
+			.frame(height: 1)
+			.padding()
+	}
+	
+	private func submit(receipt: ProcessedReceipt) async {
+		do {
+			guard let receiptSFDCId = receipt.receiptSFDCId else { return }
+			isLoading = true
+			let success = try await viewModel.submitForProcessing(receiptId: receiptSFDCId)
+			if success {
+				viewModel.receiptState = .submitted
+				try await receiptlistViewModel.getReceipts(membershipNumber: rootViewModel.member?.membershipNumber ?? "", forced: true)
+			}
+			isLoading = false
+		} catch {
+			isLoading = false
+		}
+	}
+	
+	func submitButton(receipt: ProcessedReceipt) -> some View {
+		Text(StringConstants.Receipts.submitButton)
+			.onTapGesture {
+				Task {
+					await submit(receipt: receipt)
+				}
+			}
+			.longFlexibleButtonStyle()
+			.accessibilityIdentifier(AppAccessibilty.Receipts.submitReceiptButton)
+	}
+	
+	var tryAgainButton: some View {
+		Button(StringConstants.Receipts.tryAgainButton) {
+			Task {
+				routerPath.presentedSheet = nil
+				do {
+					try await receiptlistViewModel.getReceipts(membershipNumber: rootViewModel.member?.membershipNumber ?? "", forced: true)
+				}
+			}
+			DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+				cameraModel.showCamera = true
+			}
+		}
+		.foregroundColor(.black)
+		.accessibilityIdentifier(AppAccessibilty.Receipts.tryAgainButtonProcessedReceipt)
+	}
+	
+	var errorView: some View {
+		VStack {
+			Spacer()
+			ProcessingErrorView(message1: StringConstants.Receipts.processingErrorMessageLine1,
+								message2: StringConstants.Receipts.processingErrorMessageLine2)
+			Spacer()
+			Spacer()
+			Text(StringConstants.Receipts.tryAgainButton)
+				.onTapGesture {
+					// button action
+					routerPath.presentedSheet = nil
+					DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+						cameraModel.showCamera = true
+					}
+				}
+				.longFlexibleButtonStyle()
+			Button {
+				routerPath.presentedSheet = nil
+			} label: {
+				Text(StringConstants.Receipts.backButton)
+					.foregroundColor(.black)
+			}
+			.padding(.bottom, 20)
+			.accessibilityIdentifier(AppAccessibilty.Receipts.backButton)
+		}
 	}
 }
 
